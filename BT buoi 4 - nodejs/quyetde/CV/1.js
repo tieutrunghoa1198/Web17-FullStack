aecd084b02cf14ee6b743ad6c71caba17aa9181a
Tieu Trung Hoa <tieutrunghoa1198@gmail.com>
2018-12-18 13:46:53 +0700

    Bai tap buoi 4

---
+++ BT buoi 4 - nodejs/quyetde/CV/1.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
+auto();
+function auto() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    if(slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex - 1].style.display = "block";
+    slideIndex++;
+    setTimeout(auto,5000);
+}
---
+++ BT buoi 4 - nodejs/quyetde/server.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
+//request GET => httt://localhost:6969/
+
+app.use(express.static('../quyetde/Project'));
+
+app.get('/', (req, resp) => {
+    // resp.send('chao ba con');
+    resp.send(JSON.stringify({a: 4,b: 2}) + Math.random() );
+});
+
+app.get('/about', (req, resp) => {
+    //show ra trang CV 
+    resp.sendFile(path.resolve(__dirname, './CV/project.html'));
+});
+
+
+
+app.listen(8080);
+console.log('Running at port 8080');
